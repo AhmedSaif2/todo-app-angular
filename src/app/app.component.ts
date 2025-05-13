0eb211e90fbe901bd9654ef21f9f35d55ab85bed
asaif <asaif@sumerge.com>
2025-05-13 15:27:40 +0300

    add html and css from the previous project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, TasksComponent, NewTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
